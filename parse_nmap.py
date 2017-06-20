85a704afb1e6f9a6f545d00eb7b3628e8771ce06
krubottom <karl.rubottom@gmail.com>
2017-06-20 16:23:00 -0600

    nmap parse

--- parse_nmap.py
+++ parse_nmap.py
@@ -14,78 +14,75 @@ def main():
     parser.add_argument("-f",   dest="nmap_file", required=True, help="Nmap XML File to parse" )
     arguments = parser.parse_args()
     f = open(arguments.nmap_file,"r")
-    # print f.read()
     parse_nmap(f.read())
     f.close()
 
 def parse_nmap(results_file):
 
+    nmap_dict = {}
+
     root = ET.fromstring(results_file)
 
-    for child in root:
-        print child.tag
+    for host in root.iter('host'):
+        ip_address = host[1].get('addr')
+        for osmatch in host.iter('osmatch'):
+            host_os = osmatch.get('name')
 
-    serv_dict = {}
-    ip_address = "test"
+    print ip_address, host_os
 
-    lines = results_file.split("\n")
-    for line in lines:
-        ports = []
-        line = line.strip()
-        if ("tcp" in line) and ("open" in line) and not ("Discovered" in line):
-            while "  " in line:
-                line = line.replace("  ", " ");
-            service = line.split(" ")[2]
-            port = line.split(" ")[0]
+    for port in root.iter('port'):
+        ports=[]
+        nmap_port = port.get('portid')
+        for state in port.iter('state'):
+            nmap_state = state.get('state')
+        for service in port.iter('service'):
+            nmap_service = service.get('name')
+            if service.get('version'):
+                nmap_version = service.get('version')
 
-            if service in serv_dict:
-                ports = serv_dict[service]
+        if nmap_service in nmap_dict:
+            ports = nmap_dict[nmap_service]
 
-            ports.append(port)
-            serv_dict[service] = ports
+        ports.append(nmap_port)
+        nmap_dict[nmap_service] = ports
 
-    for serv in serv_dict:
-        ports = serv_dict[serv]
+    # print nmap_dict
+
+    for serv in nmap_dict:
+        ports = nmap_dict[serv]
         if ("ftp" in serv):
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found FTP service on %s:%s" % (ip_address, port))
         elif (serv == "http") or (serv == "ssl/http") or ("https" in serv) or ("http" in serv):
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found HTTP service on %s:%s" % (ip_address, port))
         elif "mysql" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found mysql service on %s:%s" % (ip_address, port))
         elif "telnet" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found telnet service on %s:%s" % (ip_address, port))
         elif "microsoft-ds" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found MS SMB service on %s:%s" % (ip_address, port))
         elif "ms-sql" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found MS SQL service on %s:%s" % (ip_address, port))
         elif ("msdrdp" in serv) or ("ms-wbt-server" in serv):
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found RDP service on %s:%s" % (ip_address, port))
         elif "smtp" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found SMTP service on %s:%s" % (ip_address, port))
         elif "snmp" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found SNMP service on %s:%s" % (ip_address, port))
         elif "ssh" in serv:
             for port in ports:
-                port = port.split("/")[0]
                 print("   [>] Found SSH service on %s:%s" % (ip_address, port))
+        elif "unknown" in serv:
+            for port in ports:
+                print("   [>] Found Unknown service on %s:%s" % (ip_address, port))
 
 
 if __name__ == "__main__":
